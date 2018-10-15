f78a74258ef868aee0c6d677071db8bfcf917c6e
Michael Albert <mike@askkodiak.com>
2018-10-15 15:30:05 -0400

    initial version

---
+++ README.md
@@ -0,0 +1,35 @@
+# Ask Kodiak Node.js SDK Example Project - Command Line Export
+
+## Table of Contents
+
+ * [Overview](#overview)
+ * [Basic Usage](#basic-usage) 
+ * [Contributing](#contributing)
+ * [Supported Enviornments](#supported-environments) 
+ * [Documentation](#documentation)
+
+## Overview 
+
+This project uses the Ask Kodiak Node.js SDK to export product from Ask Kodiak as a CSV file. The results are echoed into the console, and you can copy and paste them into excel or your favorite spreadsheet program.
+
+## Basic Usage
+
+```bash
+
+node export-product.js --gid="yourGroupId" --key="yourAPIKey" --pid="aProductId"
+
+```
+
+## Contributing
+
+Please refer to the [CONTRIBUTING page](./CONTRIBUTING.md) for more information
+about how you can contribute to this project. We welcome bug reports, feature
+requests, code review feedback, and also pull requests.
+
+## Supported Environments
+
+The Ask Kodiak Node.js SDK supports Node.js version 6.0 and higher.
+
+## Documentation 
+
+TODO
---
+++ export-product.js
@@ -0,0 +1,72 @@
+/* eslint-disable no-console */
+const AskKodiak = require('ask-kodiak-sdk'),
+      argv = require('yargs').argv, //command line arguments
+      Json2csvParser = require('json2csv').Parser, //json to csv library
+      fs = require('fs'), //for writing the file
+      file = 'output.csv'; // name of file
+
+var precision = 'national-industry';
+
+// test for required arguments
+if (!argv.key) {
+  console.log('an api key is required.');
+  process.exit();
+}
+if (!argv.gid) {
+  console.log('an api group id (gid) is required.');
+  process.exit();
+}
+if (!argv.pid) {
+  console.log('a product id (pid) is required.');
+  process.exit();
+}
+if (argv.env) {
+  console.log('a product id (pid) is required.');
+  process.exit();
+}
+
+// does the work of turning the data into a CSV once it's an array of rows
+function rows2csv(rows) {
+  var fields = ['code', 'description', 'eligibility'],
+      opts = {fields},
+      parser = new Json2csvParser(opts);
+
+  return parser.parse(rows);
+}
+
+// init the Ask Kodiak SDK with the keys provided.
+AskKodiak.init(argv.gid, argv.key, 'http://localhost:8080/v1');
+
+AskKodiak.getEligibilityByNaicsGroupType(argv.pid, precision).then(function (eligibility) {
+  AskKodiak.getNaicsSummaryForGroupType(precision).then(function (naicsTypeSummary) {
+    var codes = Object.keys(naicsTypeSummary),
+        rows = [];
+
+    codes.forEach(function (code) {
+      var eligibilityForCode = eligibility[code] || {},
+          coverage = ((eligibilityForCode.coverage || 0) * 100) + '%';
+
+      rows.push({
+        code: code,
+        description: naicsTypeSummary[code],
+        eligibility: coverage
+      });
+    });
+
+    fs.writeFile(file, rows2csv(rows), function (error) {
+      if (error) {
+        console.error(error);
+        process.exit();
+      }
+      console.log('file saved as ' + file);
+      process.exit();
+    });
+
+  }).catch(function (error) {
+    console.error(error);
+    process.exit();
+  });
+}).catch(function (error) {
+  console.log(error);
+  process.exit();
+});
